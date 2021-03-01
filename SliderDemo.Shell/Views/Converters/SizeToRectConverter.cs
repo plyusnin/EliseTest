6f31e1e771242b8767802046a7f6816bd686a2e3
Evgenii Pliusnin <eninbox@gmail.com>
2021-03-01 20:36:29 +0500

    Added controls themes which are look similar to the screenshot

---
+++ SliderDemo.Shell/Views/Converters/SizeToRectConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using System.Windows;
+using System.Windows.Data;
+
+namespace SliderDemo.Shell.Views.Converters
+{
+    public class SizeToRectConverter : IMultiValueConverter
+    {
+        public static SizeToRectConverter Instance { get; } = new();
+
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (values.Any(v => v == DependencyProperty.UnsetValue))
+                return new Rect();
+            
+            if (values.Length != 2 || !(values[0] is double) || !(values[1] is double))
+                throw new ArgumentException(
+                    $"\"{nameof(values)}\" should contain 2 double values for width and height correspondingly");
+
+            var width  = (double) values[0];
+            var height = (double) values[1];
+            return new Rect(new Size(width, height));
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
